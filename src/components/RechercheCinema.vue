b462d5b7f766ed241dd903877293146b9f5f2401
yamissafof <yfofana@enc-sio.fr>
2024-03-19 11:37:06 +0100

    Affichage de tout les films avec un nom similaire dans RechercheCinema.vue

--- src/components/RechercheCinema.vue
+++ src/components/RechercheCinema.vue
@@ -1,38 +1,52 @@
+<template>
+  <div>
+    <input type="text" v-model="searchQuery" @keydown.enter="search">
+    <button @click="search">Rechercher</button>
+    <div v-if="searchResults.length > 0">
+      <h2>Résultats de la recherche :</h2>
+      <ul>
+        <li v-for="movie in searchResults" :key="movie.id">
+          <h3>{{ movie.title }}</h3>
+          <p>{{ movie.overview }}</p>
+          <img v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title">
+          <p v-else>Image non disponible</p>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>Aucun résultat trouvé.</p>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
-      movieName: '',
-      movieData: null
+      searchQuery: '',
+      movies: [] // Données de l'API
     };
   },
+  computed: {
+    searchResults() {
+      return this.movies.filter(movie => movie.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
+    }
+  },
   methods: {
-    searchMovie() {
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=cd101fb04d4a37358fa5ae22d9ad8e6e&query=${this.movieName}`)
-          .then(response => response.json())
-          .then(data => {
-            this.movieData = data.results[0]; // Récupérer le premier résultat (ou adapter selon les besoins)
-          })
-          .catch(error => {
-            console.error('Erreur lors de la récupération des données du film :', error);
-          });
+    async search() {
+      if (this.searchQuery.trim() === '') {
+        return; // Ne rien faire si la requête est vide
+      }
+
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=cd101fb04d4a37358fa5ae22d9ad8e6e&query=${this.searchQuery}`);
+        const data = await response.json();
+        this.movies = data.results;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données de l\'API :', error);
+        // Gérer les erreurs de récupération des données
+      }
     }
   }
 }
 </script>
-
-<template>
-    <div>
-      <label for="movieName">Entrez le nom du film :</label>
-      <input type="text" id="movieName" v-model="movieName">
-      <button @click="searchMovie">Rechercher</button>
-      <div v-if="movieData">
-        <h2>{{ movieData.title }}</h2>
-        <p>{{ movieData.overview }}</p>
-        <img :src="'https://image.tmdb.org/t/p/w500/' + movieData.poster_path" :alt="movieData.title">
-      </div>
-    </div>
-</template>
-
-<style scoped>
-</style>
