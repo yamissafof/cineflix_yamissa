c0d2204ae783dedc1094504d589ae9815f8f5627
yamissafof <yamissa.fof@gmail.com>
2024-03-30 14:25:36 +0100

    modif router.js

--- src/router/router.js
+++ src/router/router.js
@@ -20,7 +20,7 @@ const router = createRouter({
         },
         {
             path: '/movie/:id',
-            name: 'MovieDetails',
+            name: 'movieDetails',
             component: DetailCinema
         },
 
